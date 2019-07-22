421591e81f40b43aaa55b2f0fc4a11ac83980c0a
Renan <rpedrodasilva10@gmail.com>
2019-07-21 23:40:16 -0300

    README.md updated, created namespace (swagger)

--- website_api/__init__.py
+++ website_api/__init__.py
@@ -2,17 +2,19 @@ import os
 import markdown
 import csv
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
+from flask_restplus import Resource, Api, namespace
 from load_db import get_db
 
 app = Flask(__name__)
 api = Api(app)
 
+ns = api.namespace('', description='Website Integration')
 
+
+@ns.route('/company')
 class Company(Resource):
     """Entity to retrieve and update data about companies"""
-    @app.route("/")
+    @app.route("/readme")
     def index():
         """Index will show the documentation"""
         with open("./README.md", 'r') as markdown_file:
@@ -30,7 +32,7 @@ class Company(Resource):
         zipcode = request.args.get('zipcode', None)
 
         if part_name is None or zipcode is None:
-            return {'message': 'company_name and zipcode are required args', 'data': []}, 422
+            return {'message': 'company_name and zipcode are required arguments', 'data': []}, 422
         else:
             for row in conn.execute("SELECT * FROM companies WHERE company_name like ? AND zipcode like ?", ('%'+part_name+'%', zipcode)):
                 companies.append(
@@ -51,8 +53,8 @@ class Company(Resource):
         csvpath = request.args.get('csvpath', '')
         rv = self.set_website(csvpath)
         if len(rv) > 0:
-            return {'message': 'Success', 'data': rv}, 200
-        return {'message': 'Fail', 'data': rv}, 400
+            return {'message': 'Success', 'updated': rv}, 200
+        return {'message': 'Cannot process the given file', 'updated': rv}, 400
 
     def set_website(self, csv_path):
         """Updates website field from companies within the given csv"""
@@ -61,21 +63,19 @@ class Company(Resource):
         with open(csv_path) as f:
             csvFile = csv.reader(f, delimiter=";")
             for row in csvFile:
-                name, website = row[0].upper(), row[2]
+                name, zipcode, website = row[0].upper(), row[1], row[2]
                 if name != "NAME":
                     cursor = conn.execute(
-                        "SELECT company_name FROM companies WHERE company_name = ?", (name))
+                        "SELECT company_name FROM companies WHERE company_name = ? AND zipcode = ?", (name, zipcode))
 
                     if cursor.fetchone() is not None:
                         conn.execute(
-                            "UPDATE companies SET website = ? WHERE company_name = ?", (website, name,))
+                            "UPDATE companies SET website = ? WHERE company_name = ? AND zipcode = ?", (website, name, zipcode,))
 
                         # Controls updated keys
                         updated.append(
-                            {'company_name': name, 'website': website}
+                            {'company_name': name, 'zipcode': zipcode,
+                                'website': website}
                         )
 
         return updated
-
-
-api.add_resource(Company, '/company')
